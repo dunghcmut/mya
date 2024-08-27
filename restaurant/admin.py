162c46405072bde2ec31c330808be6672a49f742
dunghcmut <letrananhdung234@gmail.com>
2024-08-27 08:15:17 +0700

    jello

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BookingTable, Menu
 
+admin.site.register(BookingTable)
+admin.site.register(Menu)
 # Register your models here.
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class BookingTable(models.Model):
+    name = models.CharField(max_length=100,default="Null")
+    no_of_guest = models.SmallIntegerField()
+    BookingDate = models.DateField()
+    def __str__(self):
+        return self.name
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    def __str__(self):
+        return self.title
+    
