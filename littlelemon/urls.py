3ac3fbbbfd28fc23b6ae428eb062b56631f1387b
dunghcmut <letrananhdung234@gmail.com>
2024-08-29 16:05:29 +0700

    j

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),  # Maps the admin interface to /admin/
     path(
-        "restaurant/", include("restaurant.urls")
+        "api/", include("restaurant.urls")
     ),  # Includes URLs from the restaurant app
 ]
